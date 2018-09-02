2ffc08bc1d8486aac1e182773556ca3d6952080b
Henry Pham <phucpebble@gmail.com>
2018-09-03 07:55:55 +1200

    Henry update slack

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.listen(PORT, function () {
 
 // This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
 app.get('/', function(req, res) {
-    res.send('Ngrok is working! Path Hit: ' + req.url);
+    res.send('Henry say Ngrok is working! Path Hit: ' + req.url);
 });
 
 // This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
@@ -62,5 +62,5 @@ app.get('/oauth', function(req, res) {
 
 // Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
 app.post('/command', function(req, res) {
-    res.send('Your ngrok tunnel is up and running!');
+    res.send('Welcome to Monitor slack bot of Group 08 !');
 });
