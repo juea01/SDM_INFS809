af62babfc4dc29e57812e9986aa975dd9a59dcb6
TonyTriVo <honghung2828@gmail.com>
2018-09-19 14:50:41 +1200

    travus

--- Test/Test.JS
+++ Test/Test.JS
@@ -3,7 +3,7 @@
     plus = require("../lib/plus");
   suite("plus", function() {
     test("2 plus 3 should equals 5", function() {
-      return assert.equal(5, plus(2, 3));
+      return assert.equal(6, plus(2, 3));
     });
   });
 }).call(this);
