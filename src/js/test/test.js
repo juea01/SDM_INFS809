137b84aebfaf7c46b0c806d9c128b62a8e87153d
TonyTriVo <honghung2828@gmail.com>
2018-10-07 22:11:57 +1300

    test

--- src/js/test/test.js
+++ src/js/test/test.js
@@ -2,7 +2,7 @@
   var assert = require("assert"),
     plus = require("../lib/plus");
   suite("plus", function() {
-    test("2 plus 3 should equals 5", function() {
+    it("Function calculator", function() {
       return assert.equal(5, plus(2, 3));
     });
   });
