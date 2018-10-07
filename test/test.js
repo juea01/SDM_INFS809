bc5d065718a6c0710cc93ceedbd607427d40f6fa
TonyTriVo <honghung2828@gmail.com>
2018-10-08 00:39:19 +1300

    Update test/test.js

--- test/test.js
+++ test/test.js
@@ -27,7 +27,7 @@
 	var assert = require("assert"),
 	compares =require("../lib/Compare");
 	suite("Compare function reminder",function(){
-		it("Test compare reminder", function(){
+		it("Test compare reminder- Next reminder will <= 5 and => 50 mins", function(){
 			return assert(5 >= delay_min(2),"Min Time");
 			return assert(50 <= delay_max(50),"Max Time");
 		});
