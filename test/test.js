8406d6de91d1cd18d5d511ddc5e78cf24308578c
TonyTriVo <honghung2828@gmail.com>
2018-10-07 23:10:10 +1300

    test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
+ (function() {
  var assert = require("assert"),
-   
-(function() {
- plus = require("../lib/plus"),	
+ plus = require("../lib/plus");
  suite("plus", function() {
     it("Calculator-test-function", function() {
       return assert.equal(5, plus(2, 3));
@@ -9,7 +8,8 @@
   });
 }).call(this);
   (function() {
-  minus = require("../lib/minus"),	  
+ var assert = require("assert"),
+  minus = require("../lib/minus");	  
   suite("minus", function() {
 	 it("Canculator-check-function",function() {
 	  return assert.equal(1, minus(10,9));
