67fc26a638af35f8315b29cdc2c2d083904dac9b
TonyTriVo <honghung2828@gmail.com>
2018-10-07 22:47:26 +1300

    Update test/test.js

--- test/test.js
+++ test/test.js
@@ -7,11 +7,11 @@
       return assert.equal(5, plus(2, 3));
     });
   });
-}).call(plus);
+}).call(this);
   (function() {
   suite("minus", function() {
 	 it("Canculator-check-function",function() {
 	  return assert.equal(1, minus(10,9));
 	 });
 });
-}).call(minus);
+}).call(this);
